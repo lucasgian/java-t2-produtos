1190e5ffd399bd516641c1b1c5a40dd87b3e0a97
Gian Lucas <gianextensao@gmail.com>
2023-05-17 20:04:25 -0300

    inicio do projeto

---
+++ src/src/main/java/br/com/vendas/src/EstoqueDeProdutosApplication.java
@@ -0,0 +1,13 @@
+package br.com.vendas.src;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoqueDeProdutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoqueDeProdutosApplication.class, args);
+	}
+
+}
---
+++ src/src/test/java/br/com/vendas/src/EstoqueDeProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vendas.src;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoqueDeProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
